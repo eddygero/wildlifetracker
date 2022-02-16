734264cdd3286f94942e9bb707a4203c376c1fe1
eddygero <gero.temitayo33@gmail.com>
2022-02-16 23:45:06 +0300

    updated new commit

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,6 +1,7 @@
 import org.sql2o.Sql2o;
 
-public class DatabaseRule {
+public class
+DatabaseRule {
 
     public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "postgres", "edwardgero33");
 }
